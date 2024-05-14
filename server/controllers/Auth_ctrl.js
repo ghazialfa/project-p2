729afa030dc574516a2e6867d0a56c7750f754d4
ghazialfa <ghazialfawwaz01@gmail.com>
2024-05-14 15:04:19 +0700

    feat: login

--- server/controllers/Auth_ctrl.js
+++ server/controllers/Auth_ctrl.js
@@ -1,4 +1,7 @@
 "use strict";
+const { Op } = require("sequelize");
+const { comparePassword } = require("../helpers/bcrypt");
+const { createToken } = require("../helpers/jwt");
 const { User } = require("../models");
 
 class Auth_ctrl {
@@ -13,6 +16,48 @@ class Auth_ctrl {
         .json({ message: "User Created", user: { username, email } });
     } catch (error) {
       console.log("🚀 ~ Auth_ctrl ~ register ~ error:", error);
+      next(error);
+    }
+  }
+
+  //* ─── Login ───────────────────────────────────────────────────────────
+  static async login(req, res, next) {
+    try {
+      const { emailOrUsername, password } = req.body;
+
+      if (!emailOrUsername) {
+        throw { status: 400, message: "Email or Username is required" };
+      }
+
+      if (!password) {
+        throw { status: 400, message: "Password is required" };
+      }
+
+      const user = await User.findOne({
+        where: {
+          [Op.or]: [{ email: emailOrUsername }, { username: emailOrUsername }],
+        },
+      });
+
+      if (!user) {
+        throw { status: 401, message: "Invalid email/username or password" };
+      }
+
+      const isPasswordValid = await comparePassword(password, user.password);
+
+      if (!isPasswordValid) {
+        throw { status: 401, message: "Invalid email/username or password" };
+      }
+
+      const access_token = createToken({ id: user.id });
+
+      res.status(200).json({
+        message: "Login Success",
+        user: { username: user.username, email: user.email },
+        access_token,
+      });
+    } catch (error) {
+      console.log("🚀 ~ login ~ error:", error);
       next(error);
     }
   }
--- server/routers/index.js
+++ server/routers/index.js
@@ -4,6 +4,6 @@ const Auth_ctrl = require("../controllers/Auth_ctrl");
 const router = express.Router();
 
 router.post("/register", Auth_ctrl.register);
-router.post("/login");
+router.post("/login", Auth_ctrl.login);
 
 module.exports = router;
