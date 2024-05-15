e03d97b054322c3bdce4a671476f02303a01e651
ghazialfa <ghazialfawwaz01@gmail.com>
2024-05-16 03:51:18 +0700

    feat: ai recommendation

--- server/controllers/Movies_ctrl.js
+++ server/controllers/Movies_ctrl.js
@@ -1,5 +1,12 @@
 "use strict";
 const tmdbAPI = require("../helpers/axios");
+// const { default: OpenAI } = require("openai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// const openai = new OpenAI({
+//   api_Key: process.env.OPENAI_API_KEY,
+//   // base_url: "https://api.aimlapi.com/",
+// });
 
 class Movies_ctrl {
   //* ─── Get All ─────────────────────────────────────────────────────────
@@ -62,6 +69,113 @@ class Movies_ctrl {
         "🚀 ~ Movies_ctrl ~ getDetail ~ error:",
         error.response.data.status_message
       );
+      next(error);
+    }
+  }
+
+  //* ─── Ai Recommendation ───────────────────────────────────────────────
+  static async getAi(req, res, next) {
+    //! ─── Openai ──────────────────────────────────────────────────
+    // try {
+    //   const { request } = req.body;
+
+    //   if (!openai.apiKey) {
+    //     throw new Error("OpenAI API key is not configured.");
+    //   }
+
+    //   const completion = await openai.chat.completions.create({
+    //     messages: [
+    //       {
+    //         role: "system",
+    //         content:
+    //           "kamu adalah seorang yang ahli dalam film, dan setiap ditanya kamu menjawab tanpa respon yang lain dan hanya memberikan array judul movie yang sesuai merujuk kepada tmdb",
+    //       },
+    //       { role: "user", content: request },
+    //       // {
+    //       //   role: "assistant",
+    //       //   content: "The Los Angeles Dodgers won the World Series in 2020.",
+    //       // },
+    //       // { role: "user", content: "Where was it played?" },
+    //     ],
+    //     model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    //   });
+    //   console.log("🚀 ~ Movies_ctrl ~ getAi ~ completion:", completion);
+
+    //   console.log(completion.choices[0]);
+
+    //   res.status(200).json(completion.choices[0].message.content);
+    // } catch (error) {
+    //   console.log("🚀 ~ MoviesController ~ getAi ~ error:", error);
+    //   next(error);
+    // }
+
+    //? ─── Google ──────────────────────────────────────────────────
+    try {
+      const { userRequest } = req.body;
+      console.log("🚀 ~ Movies_ctrl ~ getAi ~ userRequest:", userRequest);
+
+      const genAI = new GoogleGenerativeAI(process.env.GOOGLE_STUDIO_API_KEY);
+
+      const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-flash-latest",
+      });
+
+      const chat = model.startChat({
+        history: [
+          {
+            role: "user",
+            parts: [
+              {
+                text: "kamu adalah seorang yang ahli dalam film, dan setiap ditanya kamu menjawab tanpa respon yang lain dan hanya memberikan array judul movie yang sesuai merujuk kepada tmdb",
+              },
+            ],
+          },
+          {
+            role: "model",
+            parts: [{ text: "Oke, mengerti. Beri aku pertanyaanmu." }],
+          },
+        ],
+        generationConfig: {
+          maxOutputTokens: 100,
+        },
+      });
+
+      const msg = userRequest;
+      const result = await chat.sendMessage(msg);
+      const response = await result.response;
+      const text = await response.text();
+      // console.log("🚀 ~ Movies_ctrl ~ getAi ~ text:", text);
+
+      const movies = [];
+
+      for (const movie of JSON.parse(text)) {
+        const { data } = await tmdbAPI.get("/search/movie", {
+          params: {
+            query: movie.trim(),
+          },
+        });
+        if (data.results.length > 0) {
+          movies.push(data.results[0]);
+        }
+      }
+
+      const output = movies.map((el) => ({
+        id: el.id,
+        title: el.title,
+        backdrop_path: el.backdrop_path,
+        genres: el.genres,
+        original_language: el.original_language,
+        overview: el.overview,
+        vote_count: el.vote_count,
+        vote_average: el.vote_average,
+        poster_path: el.poster_path,
+        release_date: el.release_date,
+        adult: el.adult,
+      }));
+
+      res.status(200).json({ response: output });
+    } catch (error) {
+      console.log("🚀 ~ Movies_ctrl ~ getAi ~ error:", error.message);
       next(error);
     }
   }
